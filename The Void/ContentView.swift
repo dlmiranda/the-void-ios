df170894289fd518fd76cd63a30f5b6208707763
David Miranda <d.miranda@me.com>
2026-04-15 20:38:46 -0700

    Initial Commit

---
+++ The Void/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Void
+//
+//  Created by David Miranda on 4/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Void/The_VoidApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_VoidApp.swift
+//  The Void
+//
+//  Created by David Miranda on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct The_VoidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
